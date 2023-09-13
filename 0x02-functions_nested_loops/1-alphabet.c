ad6b3dbdba4699afe0a37a6e054e435c7075feda
Ammar <ammar@haddad.com>
2023-09-12 19:16:33 -0700

    Done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - Printing all of the alphabets in lowercase with a new line at the end
+ * print_alphabet - Printing all of the alphabets 
+ * in lowercase with a new line at the end
  */
 void print_alphabet(void)
 {
