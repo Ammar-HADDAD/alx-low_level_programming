ce5276242227f04831bc289f3eb0acadbe66edfe
Ammar <ammar@haddad.com>
2023-09-14 18:24:31 -0700

    Done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,8 @@
 
 /**
  * _isupper - checking for uppercase character
- * @c (int) - the input character
+ * @c (int) : the input character
+ *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
